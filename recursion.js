eaaf7b73279efa3629acc85973807398024a8ff1
marcuses101 <mnjconnolly@gmail.com>
2021-01-22 13:53:35 -0500

    add iterative

--- recursion.js
+++ recursion.js
@@ -1,163 +1,332 @@
-// Input is a number
-// Output is a string
-// Input of recursive call is the previous iteration's number minus one
-// Output of recursive call is a string.
-
-function countingSheep(num) {
-  if (num === 0) return console.log(`All sheep jumped over the fence`);
-  console.log(`${num} Another sheep jumps over the fence`);
-  countingSheep(num - 1);
-}
-
-function powerCalculator(integer, exponent) {
-  if (exponent < 0) return console.log(`exponent should be >=0`);
-  if (exponent === 0) return 1;
-  return integer * powerCalculator(integer, exponent - 1);
-}
-
-function reverseString(string) {
-  if (string.length === 1) return string;
-  return string.slice(-1) + reverseString(string.slice(0, string.length - 1));
-}
-
-function triangularNumber(integer) {
-  if (integer === 0) return 0;
-  return integer + triangularNumber(integer - 1);
-}
-
-function stringSplitter(string, separator) {
-  const splitArray = [];
-  function split(string, separator, acc = "") {
-    if (string[0] === separator) {
-      splitArray.push(acc);
-      return split(string.slice(1), separator, "");
+module.exports = {
+  countingSheep(num) {
+    if (num === 0) return console.log(`All sheep jumped over the fence`);
+    console.log(`${num} Another sheep jumps over the fence`);
+    this.countingSheep(num - 1);
+  },
+  countingSheepIteratively(num) {
+    for (let i = num; i > 0; i--) {
+      console.log(`${i} Another sheep jumps over the fence`);
     }
-    if (!string) return splitArray.push(acc);
-    return split(string.slice(1), separator, acc + string[0]);
-  }
-  split(string, separator);
-  return splitArray;
-}
+    console.log(`All the sheep jumped over the fence`);
+  },
 
-function fibonacci(positiveInteger) {
-  if (positiveInteger === 1) return 1;
-  return positiveInteger * fibonacci(positiveInteger - 1);
-}
+  powerCalculator(integer, exponent) {
+    if (exponent < 0) return console.log(`exponent should be >=0`);
+    if (exponent === 0) return 1;
+    return integer * this.powerCalculator(integer, exponent - 1);
+  },
 
-// input is a 2d array and current position.
-function mazeEscaper(maze, position = [0, 0]) {
-  const [row, column] = position;
-  const targetRow = maze.length - 1;
-  const targetColumn = maze[0].length - 1;
-  if (row === targetRow && column === targetColumn) return "";
-  maze[row][column] = "*";
-
-  if (maze[row][column + 1] && maze[row][column + 1] !== "*") {
-    return "R" + mazeEscaper(maze, [row, column + 1]);
-  } else if (maze[row + 1][column] && maze[row + 1][column] !== "*") {
-    return "D" + mazeEscaper(maze, [row + 1, column]);
-  } else if (maze[row][column - 1] && maze[row][column - 1] !== "*") {
-    return "L" + mazeEscaper(maze, [row, column - 1]);
-  } else if (maze[row - 1][column] && maze[row - 1][column] !== "*") {
-    return "U" + mazeEscaper(maze[row - 1][column]);
-  }
-}
-
-let maze = [
-  [" ", " ", " ", "*", " ", " ", " "],
-  ["*", "*", " ", "*", " ", "*", " "],
-  [" ", " ", " ", " ", " ", " ", " "],
-  [" ", "*", "*", "*", "*", "*", " "],
-  [" ", " ", " ", " ", " ", " ", "e"],
-];
-
-function allPaths(maze, position = [0, 0], pathString = "") {
-  const [row, column] = position;
-  const possibleNextPosition = [];
-  if (maze[row][column] === "e")
-    return console.log(`Path to the exit ${pathString}`);
-
-  if (maze[row]?.[column + 1] && maze[row][column + 1] !== "*") {
-    possibleNextPosition.push({
-      string: "R",
-      position: [row, column + 1],
-    });
-  }
-  if (maze[row + 1]?.[column] && maze[row + 1][column] !== "*") {
-    possibleNextPosition.push({
-      string: "D",
-      position: [row + 1, column],
-    });
-  }
-  if (maze[row]?.[column - 1] && maze[row][column - 1] !== "*") {
-    possibleNextPosition.push({
-      string: "L",
-      position: [row, column - 1],
-    });
-  }
-  if (maze[row - 1]?.[column] && maze[row - 1][column] !== "*") {
-    possibleNextPosition.push({ string: "U", position: [row - 1, column] });
-  }
-
-  possibleNextPosition.forEach((nextPosition) => {
-    const newMaze = maze.map((row) => [...row]);
-    newMaze[row][column] = "*";
-    const path = pathString + nextPosition.string;
-    allPaths(newMaze, nextPosition.position, path);
-  });
-}
-
-function anagrams(string) {
-  // loop through each of the characters of the string
-  const answers = [];
-  function traverse(string, perm = "") {
-    if (!string) answers.push(perm);
-    for (let i = 0; i < string.length; i++) {
-      traverse(string.slice(0, i) + string.slice(i + 1), perm + string[i]);
+  powerCalculatorIterative(integer, exponent) {
+    if (exponent < 0) return console.log(`exponent should be >= 0 `);
+    let result = integer;
+    for (let i = 1; i < exponent; i++) {
+      result = result * integer;
     }
-  }
-  traverse(string);
-  return answers;
-}
+    return result;
+  },
 
-//input object
-//output indented console log?
+  reverseString(string) {
+    if (string.length === 1) return string;
+    return (
+      string.slice(-1) + this.reverseString(string.slice(0, string.length - 1))
+    );
+  },
+  reverseStringIterative(string) {
+    const stringArray = string.split("");
+    const reversedArray = [];
+    while (stringArray.length > 0) {
+      reversedArray.push(stringArray.pop());
+    }
+    return reversedArray.join("");
+  },
+  triangularNumber(integer) {
+    if (integer === 0) return 0;
+    return integer + triangularNumber(integer - 1);
+  },
+  triangularNumberIterative(int) {
+    let triNum = 0;
+    for (let i = int; i > 0; i--) {
+      triNum += i;
+    }
+    return triNum;
+  },
 
-function organizationChart(object) {
-  let string = '';
-  function recursion(obj, indentation = ''){
-    for ([key,value] of Object.entries(obj)){
-      if (!value){string = string + indentation + key + '\n'}
-      else {
-        string = string + indentation + key + '\n';
-        recursion(value, indentation + `  `)
+  stringSplitter(string, separator) {
+    const splitArray = [];
+    function split(string, separator, acc = "") {
+      if (string[0] === separator) {
+        splitArray.push(acc);
+        return split(string.slice(1), separator, "");
+      }
+      if (!string) return splitArray.push(acc);
+      return split(string.slice(1), separator, acc + string[0]);
+    }
+    split(string, separator);
+    return splitArray;
+  },
+  stringSplitterIterative(inputString, separator) {
+    let string = "";
+    let array = [];
+    for (let i = 0; i < inputString.length; i++) {
+      if (inputString[i] === separator) {
+        array.push(string);
+        string = "";
+      } else {
+        string += inputString[i];
       }
     }
-  }
-  recursion(object)
-  return string;
-}
+    array.push(string);
+    return array;
+  },
 
-function binaryRepresentation(num, binary = "") {
-  if (num === 0) return binary;
-  return binaryRepresentation(Math.floor(num / 2), (num % 2) + binary);
-}
+  fibonacci(integer) {
+    function fib(array) {
+      if (array.length === integer) return array;
+      const current =
+        (array[array.length - 1] ?? 1) + (array[array.length - 2] ?? 0);
+      console.log(current);
+      return fib([...array, current]);
+    }
+    return fib([]);
+  },
 
-const orgObject = {
-  Zuckerberg: {
-    Schroepfer: {
-      Bosworth: {
-        Steve: null,
-        Kyle: null,
-        Andra: null,
-      },
-      Zhao: {
-        Richie: null,
-        Sophia: null,
-        Jen: null,
-      },
-    },
+  fibIterative(int) {
+    const arr = [];
+    for (let i = 0; i < int; i++) {
+      const num = (arr[i - 1] ?? 1) + (arr[i - 2] ?? 0);
+      console.log(num);
+      arr.push(num);
+    }
+  },
+
+  factorial(int) {
+    if (int === 0) return 1;
+    return int * this.factorial(int - 1);
+  },
+
+  factorialIterative(int) {
+    let current = 1;
+    for (let i = 0; i < int; i++) {
+      current *= int - i;
+    }
+    return current;
+  },
+
+  mazeEscape(maze) {
+    const end = `${maze.length - 1}${maze[0].length - 1}`;
+    const mazeMap = new Map();
+    maze.forEach((rowArr, row) =>
+      rowArr.forEach((_, column) => {
+        const adjacentNodes = {
+          R: [" ", "e"].includes(maze[row]?.[column + 1])
+            ? `${row}${column + 1}`
+            : null,
+          D: [" ", "e"].includes(maze[row + 1]?.[column])
+            ? `${row + 1}${column}`
+            : null,
+          U: [" ", "e"].includes(maze[row - 1]?.[column])
+            ? `${row - 1}${column}`
+            : null,
+          L: [" ", "e"].includes(maze[row]?.[column - 1])
+            ? `${row}${column - 1}`
+            : null,
+        };
+        mazeMap.set(
+          `${row}${column}`,
+          Object.entries(adjacentNodes).filter(([key, value]) => value)
+        );
+      })
+    );
+    let pathString = "";
+    function findPath(current = "00", visited = ["00"], path = "") {
+      if (pathString) return;
+      const adjacent = mazeMap.get(current);
+      const seen = [...visited];
+      if (current === end) {
+        pathString = path;
+        return;
+      }
+      for ([direction, location] of adjacent) {
+        if (visited.includes(location)) {
+          continue;
+        }
+        const currentPath = path + direction;
+        seen.push(location);
+        findPath(location, seen, currentPath);
+      }
+    }
+    findPath();
+    return pathString;
+  },
+
+  mazeEscapeAll(maze) {
+    const end = `${maze.length - 1}${maze[0].length - 1}`;
+    const mazeMap = new Map();
+    maze.forEach((rowArr, row) =>
+      rowArr.forEach((_, column) => {
+        const adjacentNodes = {
+          R: [" ", "e"].includes(maze[row]?.[column + 1])
+            ? `${row}${column + 1}`
+            : null,
+          D: [" ", "e"].includes(maze[row + 1]?.[column])
+            ? `${row + 1}${column}`
+            : null,
+          U: [" ", "e"].includes(maze[row - 1]?.[column])
+            ? `${row - 1}${column}`
+            : null,
+          L: [" ", "e"].includes(maze[row]?.[column - 1])
+            ? `${row}${column - 1}`
+            : null,
+        };
+        mazeMap.set(
+          `${row}${column}`,
+          Object.entries(adjacentNodes).filter(([key, value]) => value)
+        );
+      })
+    );
+    const paths = [];
+    function findPaths(current = "00", visited = ["00"], path = "") {
+      const adjacent = mazeMap.get(current);
+      const seen = [...visited];
+      if (current === end) return paths.push(path);
+      for ([direction, location] of adjacent) {
+        if (visited.includes(location)) {
+          continue;
+        }
+        const currentPath = path + direction;
+        seen.push(location);
+        findPaths(location, seen, currentPath);
+      }
+    }
+    findPaths();
+    return paths;
+  },
+
+  mazeEscapeIterative(maze) {
+    const end = `${maze.length - 1}${maze[0].length - 1}`;
+    const mazeMap = new Map();
+    maze.forEach((rowArr, row) =>
+      rowArr.forEach((_, column) => {
+        const adjacentNodes = {
+          R: [" ", "e"].includes(maze[row]?.[column + 1])
+            ? `${row}${column + 1}`
+            : null,
+          D: [" ", "e"].includes(maze[row + 1]?.[column])
+            ? `${row + 1}${column}`
+            : null,
+          U: [" ", "e"].includes(maze[row - 1]?.[column])
+            ? `${row - 1}${column}`
+            : null,
+          L: [" ", "e"].includes(maze[row]?.[column - 1])
+            ? `${row}${column - 1}`
+            : null,
+        };
+
+        mazeMap.set(
+          `${row}${column}`,
+          Object.entries(adjacentNodes).filter(([key, value]) => value)
+        );
+      })
+    );
+    const queue = [{ location: "00", path: "" }];
+    const visited = [];
+    while (queue.length) {
+      const currentNode = queue.pop();
+      if (currentNode.location === end) return currentNode.path;
+      visited.push(currentNode.location);
+      const edges = mazeMap.get(currentNode.location);
+      edges.forEach(([direction, location]) => {
+        if (!visited.includes(location))
+          queue.push({ location, path: `${currentNode.path}${direction}` });
+      });
+    }
+  },
+  mazeEscapeAllIterative(maze) {
+    const end = `${maze.length - 1}${maze[0].length - 1}`;
+    const mazeMap = new Map();
+    maze.forEach((rowArr, row) =>
+      rowArr.forEach((_, column) => {
+        const adjacentNodes = {
+          R: [" ", "e"].includes(maze[row]?.[column + 1])
+            ? `${row}${column + 1}`
+            : null,
+          D: [" ", "e"].includes(maze[row + 1]?.[column])
+            ? `${row + 1}${column}`
+            : null,
+          U: [" ", "e"].includes(maze[row - 1]?.[column])
+            ? `${row - 1}${column}`
+            : null,
+          L: [" ", "e"].includes(maze[row]?.[column - 1])
+            ? `${row}${column - 1}`
+            : null,
+        };
+
+        mazeMap.set(
+          `${row}${column}`,
+          Object.entries(adjacentNodes).filter(([key, value]) => value)
+        );
+      })
+    );
+    const queue = [{ location: "00", path: "", visited: [] }];
+    const paths = [];
+    while (queue.length) {
+      const currentNode = queue.pop();
+      if (currentNode.location === end) paths.push(currentNode.path);
+      const edges = mazeMap.get(currentNode.location);
+      edges.forEach(([direction, location]) => {
+        if (!currentNode.visited.includes(location))
+          queue.push({
+            location,
+            path: `${currentNode.path}${direction}`,
+            visited: [...currentNode.visited, currentNode.location],
+          });
+      });
+    }
+    return paths;
+  },
+
+  anagrams(string) {
+    // loop through each of the characters of the string
+    const answers = [];
+    function traverse(string, perm = "") {
+      if (!string) answers.push(perm);
+      for (let i = 0; i < string.length; i++) {
+        traverse(string.slice(0, i) + string.slice(i + 1), perm + string[i]);
+      }
+    }
+    traverse(string);
+    return answers;
+  },
+
+  anagramsIterative(string){
+    const answers = new Set();
+    answers.add(string);
+    
+
+  },
+
+  //input object
+  //output indented console log?
+
+  organizationChart(object) {
+    let string = "";
+    function recursion(obj, indentation = "") {
+      for ([key, value] of Object.entries(obj)) {
+        if (!value) {
+          string = string + indentation + key + "\n";
+        } else {
+          string = string + indentation + key + "\n";
+          recursion(value, indentation + `  `);
+        }
+      }
+    }
+    recursion(object);
+    return string;
+  },
+
+  binaryRepresentation(num, binary = "") {
+    if (num === 0) return binary;
+    return binaryRepresentation(Math.floor(num / 2), (num % 2) + binary);
   },
 };
-
-console.log(organizationChart(orgObject));
